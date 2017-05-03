db8fbc55b94e9c9a6a1668e77a678b9ddc03c9b9
ahmed.saeed <ahmed.saeed@intagleo.com>
2017-05-03 15:51:15 +0500

    added back config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'dts2k.me';
+$config['base_url'] = 'http://54.167.58.68/dts2k.me/';
 
 /*
 |--------------------------------------------------------------------------
@@ -521,3 +521,4 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
