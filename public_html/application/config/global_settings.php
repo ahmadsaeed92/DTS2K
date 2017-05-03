22ec0878f408a674ea9044a6da6040b4ca586424
ahmed.saeed <ahmed.saeed@intagleo.com>
2017-05-03 14:14:17 +0500

    logo handling

--- public_html/application/config/global_settings.php
+++ public_html/application/config/global_settings.php
@@ -8,5 +8,5 @@ $config['global_settings']['date_format'] = 'm/d/Y'; // U.S Date Format
 $config['global_settings']['date_time_format'] = 'm/d/Y H:i:s';
 $config['global_settings']['store#'] = 'Store_test'; //Store Number
 
-// base_url() . "assets/images/{client's_logo}"  base_url() => "http:dts2k.me/"
-$config['client_logo_file_path'] = 'http:dts2k.me/assets/images/wendys_logo.png'; 
+// "images/{client's_logo_name}"
+$config['client_logo_file_path'] = 'images/wendys_logo.png'; 
--- public_html/application/views/templates/content_header.php
+++ public_html/application/views/templates/content_header.php
@@ -1,15 +1,17 @@
 <?php ?>
 <div class="main-container">
-<header class="header container-fluid">
-    <div class="col-md-4 col-sm-4 col-xs-12 pr">
-        <a class="nav-btn" style="display:none!important;" href="#"><img src="<?php echo asset_url() . "images/bars.png" ?>" width="25" height="21" alt=""></a>
-        <a href="#" class="logo"><img src="<?php echo asset_url() . "images/techknow.png" ?>" class="img-responsive" alt="" ></a>
-    </div>
-    <div class="col-md-4 col-sm-4 col-xs-12 text-center">
-        <a href="#"><img src="<?php echo asset_url() . "images/dts.png" ?>" class="img-responsive logo2" ></a>
-    </div>
-    <div class="col-md-4 col-sm-4 col-xs-12 ">
-        <a href="<?php echo base_url() . "index.php/logout/" ?>" class="logout"><img src="<?php echo asset_url() . "images/icon-logout.png" ?>" width="19" height="18" alt="" ></a>
-        <a href="#" class="pull-right logo3"><img src="<?php echo asset_url() . "images/wendys_logo.png" ?>" class="img-responsive" alt="" ></a>
-    </div>
-</header>
+    <header class="header container-fluid">
+        <div class="col-md-4 col-sm-4 col-xs-12 pr">
+            <a class="nav-btn" style="display:none!important;" href="#"><img src="<?php echo asset_url() . "images/bars.png" ?>" width="25" height="21" alt=""></a>
+            <a href="#" class="logo"><img src="<?php echo asset_url() . "images/techknow.png" ?>" class="img-responsive" alt="" ></a>
+        </div>
+        <div class="col-md-4 col-sm-4 col-xs-12 text-center">
+            <a href="#"><img src="<?php echo asset_url() . "images/dts.png" ?>" class="img-responsive logo2" ></a>
+        </div>
+        <div class="col-md-4 col-sm-4 col-xs-12 ">
+            <a href="<?php echo base_url() . "index.php/logout/" ?>" class="logout"><img src="<?php echo asset_url() . "images/icon-logout.png" ?>" width="19" height="18" alt="" ></a>
+            <?php if (file_exists("assets/" . $this->config->item('client_logo_file_path'))): ?>
+                <a href="#" class="pull-right logo3"><img src="<?php echo asset_url() . $this->config->item('client_logo_file_path'); ?>" class="img-responsive" alt="" ></a>
+                <?php endif; ?>
+        </div>
+    </header>
